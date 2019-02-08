1d90804652ade4db5f373c55fc011c3fdc6785d0
MPARANTHAMAN <42861773+MPARANTHAMAN@users.noreply.github.com>
2019-02-08 15:06:21 +0530

    Create armstrong no between 2 intergal.py

---
+++ armstrong no between 2 intergal.py
@@ -0,0 +1,12 @@
+a1,b1=raw_input().split()
+a1=int(a1)
+b1=int(b1)
+for num in range(a1,b1):
+	sum=0
+	temp=num
+	while temp>0:
+		digit = temp %10
+		sum =sum+digit**3
+		temp/=10
+	if	num == sum:
+	  print(num)
